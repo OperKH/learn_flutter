3ce81cbb16760a09176e7d68366729d18a030829
OperKH <oper.kh@gmail.com>
2018-11-15 16:53:20 +0200

    Basics.

---
+++ widgets_basics/lib/product_manager.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct = 'Sweets Tester'});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  final List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ProductControl(_addProduct),
+        Products(_products)
+      ],
+    );
+  }
+}
